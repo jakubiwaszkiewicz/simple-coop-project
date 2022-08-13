c6119878bc9e7f85fb2bd13aed64909e16ed974b
blacksanta <xxxsiwyx6@gmail.com>
2022-08-13 17:51:25 +0200

    cos nie cos zrobilem ale potem dokoncze, main zaczety plus home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+export default function Home(){
+    return(
+        <div id="home">
+            <div className="home--img">
+                
+            </div>
+            <div className="home--inf">
+                12ewewew
+            </div>
+        </div>
+        
+    );
+}
